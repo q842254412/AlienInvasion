d5a6ed330a628780f50f36ac88ec01ae1e7627f3
gentle <pthgentle1@live.com>
2024-03-07 09:59:38 +0800

    ship.py

--- ship.py
+++ ship.py
@@ -5,3 +5,17 @@ Created on Wed Mar  6 23:14:42 2024
 @author: gentleH
 """
 
+import pygame
+
+class Ship:
+    def __init__(self,ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        
+        self.image = pygame.image.load("image/ship.bmp")
+        self.rect = self.image.get_rect()
+        
+        self.rect.midbottom = self.screen_rect.midbottom
+        
+    def blitme(self):
+        self.screen.blit(self.image,self.rect)
